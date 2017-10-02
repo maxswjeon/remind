92450fa2542107fa19acdb3a1e4bdde8b11e2b9d
tmdgus0084 <tmdgus0084@naver.com>
2017-10-02 16:12:11 +0900

    1
    1

---
+++ board/project/index.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="ko">
+	<head>
+		<meta name="theme-color" content="#FFF">
+		<meta id="viewport" name="viewport" content="width=device-width, initial-scale=1">
+		<meta charset="utf-8">
+
+		<!--for iOS-->
+		<meta name="apple-mobile-web-app-capable" content="yes">
+		<meta name="mobile-web-app-capable" content="yes">
+		<meta name="apple-mobile-web-app-title" content="REMIND">
+		<link rel="apple-touch-icon" href="/assets/image/favicon.jpg">
+		<!---->
+
+		<title>REMIND</title>
+
+		<link rel="shortcut icon" href="/assets/image/favicon.jpg">
+
+		<link rel="stylesheet" type="text/css" href="/assets/fonts/NanumGothic.css"/>
+		<link rel="stylesheet" type="text/css" href="/assets/fonts/Stalinist_One.css"/>
+
+		<link rel="stylesheet" type="text/css" href="/assets/css/lib/jquery.fullpage.min.css" />
+
+		<script type="text/javascript" src="/assets/js/lib/jquery.min.js"></script>
+		<script type="text/javascript" src="/assets/js/reset.js" defer></script>
+		<script type="text/javascript" src="/assets/js/lib/jquery.fullpage.min.js"></script>
+
+		<link rel="stylesheet" type="text/css" href="/assets/css/global/reset.css"/>
+		<link rel="stylesheet" type="text/css" href="/assets/css/global/header/header.css"/>
+		<link rel="stylesheet" type="text/css" media="screen and (max-width: 500px)" href="/assets/css/global/header/mobile.css"/>
+		<link rel="stylesheet" type="text/css" media="screen and (min-width: 501px)" href="/assets/css/global/header/desktop.css"/>
+
+		<link rel="stylesheet" type="text/css" href="/assets/css/lib/button.css"/>
+		<link rel="stylesheet" type="text/css" href="/assets/css/index/index.css"/>
+		<link rel="stylesheet" type="text/css" media="screen and (max-width: 500px)" href="/assets/css/index/mobile.css"/>
+		<link rel="stylesheet" type="text/css" media="screen and (min-width: 501px)" href="/assets/css/index/desktop.css"/>
+
+		<style>
+			body {
+				background: #6693B2;
+			}
+			#contents {
+				display: block;
+				width: 90%;
+				margin: 4rem auto;
+				position: relative;
+				animation-name: focus-in;
+				animation-duration: 1s;
+				animation-fill-mode: forwards;
+			}
+			@keyframes focus-in {
+				0% {
+					opacity: 0;
+					filter: blur(3px);
+					left: 80vh;
+				}
+				100% {
+					opacity: 1;
+					filter: blur(0);
+					left: 0;
+				}
+			}
+			@media screen and (min-width: 501px) {
+				@keyframes focus-in {
+					0% {
+						opacity: 0;
+						filter: blur(3px);
+						left: 15vh;
+					}
+					100% {
+						opacity: 1;
+						filter: blur(0);
+						left: 0;
+					}
+				}
+			}
+		</style>
+
+	</head>
+	<body>
+		<header>
+			<div id="box_left">
+				<a href="/">
+					<h1 id="title">REMIND</h1>
+				</a>
+			</div>
+			<div id="box_right">
+				<ul id="menu_member">
+					<li class="item item_member"><a href="/member/signup" class="link_member">회원가입</a></li>
+					<li class="item item_member"><a href="/member/login" class="link_member">로그인</a></li>
+				</ul>
+			</div>
+		</header>
+
+		<div id="contents">
+			<nav>
+				hello
+			</nav>
+			<article>
+				<h1>hello ariticle</h1>
+				<div>
+					세계문자 가운데 한글, 즉 훈민정음은 흔히들 신비로운 문자라 부르곤 합니다. 그것은 세계 문자 가운데 유일하게 한글만이 그것을 만든 사람과 반포일을 알며, 글자를 만든 원리까지 알기 때문입니다. 세계에 이런 문자는 없습니다. 그래서 한글은, 정확히 말해 [훈민정음 해례본](국보 70호)은 진즉에 유네스코 세계기록유산으로 등재되었습니다. ‘한글’이라는 이름은 1910년대 초에 주시경 선생을 비롯한 한글학자들이 쓰기 시작한 것입니다. 여기서 ‘한’이란 크다는 것을 뜻하니, 한글은 ‘큰 글’을 말한다고 하겠습니다.[훈민정음 해례본]은 세종이 직접 서문을 쓰고 정인지 같은 신하들에게 글자에 대한 설명을 적게 한 것입니다. 이 책이 1940년에 안동에서 발견될 때까지 우리는 한글의 창제 원리에 대해 전혀 모르고 있었습니다. 그러다 이 책이 발견됨으로 해서 한글이 얼마나 과학적인 원리로 만들어졌는지 알게 되었답니다. 이 책이 우리에게 전해진 것은 전적으로 성북동에 있는 간송미술관을 세운 전형필 선생의 공입니다. 선생은 아주 비싼 가격으로 이 책을 샀고 6∙25 때에도 이 책 한 권만 들고 피난 갈 정도로 이 책을 지키기 위해 몸을 바친 분입니다. 이 분도 [직지]를 세계에 알린 박병선 선생처럼 우리의 문화영웅입니다.한국인들에게 과거 유산 가운데 가장 자랑스러운 것을 꼽으라면 열이면 열 모두 한글을 말합니다. 그 이유를 물으면대개 “모든 소리를 적을 수 있기 때문” 또는 “가장 과학적인 문자이므로”라고 말하며, 어떤 사람은 “배우기 가장 쉬운 문자라서”라고 답하기도 합니다. 그런데 이러한 대답들은 부분적으로만 사실일 뿐, 충분한 답변이 되지 못합니다.우선 한글이 모든 소리를 적을 수 있다는 것은 사실이 아닙니다. 왜냐하면 세계의 문자는 그 나라 말만 정확하게 적을 수 있기 때문입니다. 한글로는 영어의 ‘f'나 'th'를 적을 수 없지 않습니까? 그 다음으로 한글이 세계 언어학자들을 깜짝 놀라게 할 만큼 과학적인 원리로 만들어졌다는 것은 맞습니다. 그래서 미국 메릴랜드 대학에 있는 램지(Ramsey) 교수는 한글날에 학생들과 조촐한 자축연을 했답니다. 이렇게 멋진 문자가 나온 날을 축하하지 않으면 어떻게 하느냐고 말입니다. 그런데 정작 우리 한국인들은 어떤 의미에서 한글이 ‘과학적’인지 잘 모릅니다. 한글이 배우기 쉽다는 것도 이같은 ‘한글의 과학성’과 연관되기 때문에 우리는 이 부분에 대해 잘 알아야 합니다.
+				</div>
+			</article>
+		</div>
+	</body>
+</html>
--- index.php
+++ index.php
@@ -1,115 +1,160 @@
 <!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <?php $include = 1; include_once('lib/functions.php');?>
-  <!--for iOS-->
-    <meta name="apple-mobile-web-app-capable" content="yes">
-    <meta name="mobile-web-app-capable" content="yes">
-    <meta name="apple-mobile-web-app-title" content="REMIND">
-    <link rel="apple-touch-icon" href="http://remind.zz.am/ios/icon.jpg">
-  <!---->
-  <?php if(checkMobile()){ ?>
-    <link rel="stylesheet" type="text/css" href="/lib/mobile_main.css">
-  <?php }else{ ?>
-    <link rel="stylesheet" type="text/css" href="/lib/pc_main.css">
-  <?php } ?>
-  <title>REMIND</title>
-  <script src="/lib/functions.js"></script>
-</head>
+<html lang="ko">
+	<head>
+		<meta name="theme-color" content="#FFF">
+		<meta id="viewport" name="viewport" content="width=device-width, initial-scale=1">
+		<meta charset="utf-8">
 
-<body>
-  <header onclick="link('/');">
-    <h1 class="title">REMIND<br><div class="second_tile">Web Games</div></h1>
-  </header>
+		<!--for iOS-->
+		<meta name="apple-mobile-web-app-capable" content="yes">
+		<meta name="mobile-web-app-capable" content="yes">
+		<meta name="apple-mobile-web-app-title" content="REMIND">
+		<link rel="apple-touch-icon" href="/assets/image/favicon.jpg">
+		<!---->
 
-  <nav>
-    <?php if (checkMobile()){ ?>
-    <!--mobile nav-->
-    <div class="menu" onclick="link('games/bwtile?m=1');">
-      <img class="menu" src="img/main/m_bw_main.jpg">
-      <div class="text">
-        <b class="title">Game: BW tiles</b>
-        흑백의 타일들을 조작하여 화면을 모두 하얗게, 까맣게 만들어 버리세요!
-      </div>
-    </div>
-    <div class="menu" onclick="link('games/new_game?m=1');">
-      <img class="menu" src="img/main/iconmonstr-light-bulb-18-240.png">
-      <div class="text">
-        <b class="title">New Game!</b>
-        새로운 게임이 개발중입니다. 기대해주십시오!
-      </div>
-    </div>
-    <div class="menu" onclick="now_dev_alert();">
-      <img class="menu" src="img/main/m_user.jpg">
-      <div class="text">
-        <b class="title">사용자 지원</b>
-        사용자의 편의를 위한 몇 가지 기능들을 소개합니다.
-      </div>
-    </div>
-    <div class="menu" onclick="link('dev?m=1');">
-      <img class="menu" src="img/main/Gear_develop_Mobile.jpg">
-      <div class="text">
-        <b class="title">개발자 지원</b>
-        이 페이지의 개발 정보를 얻습니다. 게임의 소스 코드 등을 얻을 수 있습니다.
-      </div>
-    </div>
-    <?php }else{ ?>
-    <!--pc nav-->
-      <div class="polaroid" onclick="link('games/bwtile?pc=1');">
-        <img src="img/main/bw_main.jpg">
-        <div class="container">
-          <p><h2>Game: BW tiles</h2><br>
-            흑백의 타일들을 조작하여 화면을 모두 하얗게, 까맣게 만들어 버리세요!
-          </p>
-        </div>
-      </div>
-      <div class="polaroid" onclick="now_dev_alert();">
-        <img src="img/main/user.png">
-        <div class="container">
-          <p><h2>사용자 지원</h2><br>
-            사용자의 편의를 위한 몇 가지 기능들을 소개합니다.
-          </p>
-        </div>
-      </div>
-      <div class="polaroid" onclick="link('dev?pc=1');">
-        <img src="img/main/gear.png">
-        <div class="container">
-          <p><h2>개발자 지원</h2><br>
-            이 페이지의 개발 정보를 얻습니다. 게임의 소스 코드 등을 얻을 수 있습니다.
-          </p>
-        </div>
-      </div>
-    <?php } ?>
-  </nav>
+		<title>REMIND</title>
 
-    <article>
-      <div class="no_supprot">
-      사용자의 해상도는 지원되지 않는 해상도입니다. <a href="?m=1">여기를 눌러</a> 모바일 버전으로 접속해주시기 바랍니다.
-      </div>
-      <div class="ui container" style="border-top: 2px solid #3F3F3F;margin-top:1rem;">
-        <div class="text_post" style="margin-top:1rem;">
-          <div class="left text">
-            <h1 class="center">여러분들께 웹 게임의 발전 가능성을 제시합니다.</h1>
-            우리는 어릴 적 웹에 있는 플래시 게임들을 즐겨 했습니다. 저 또한 그런 플래시 게임들을 즐겨 했습니다.
-            친구들과 집에 모이면 하나의 컴퓨터로 2인용 게임을 하기도 하였고 혼자서도 재미있게 게임을 즐길 수 있었습니다.
-            그래서 우리는 그때의 기억을 다시금 떠올리게 하고자 웹 게임을 개발하게 되었습니다.
-            요즘은 PC게임의 발전으로 웹 게임은 조금씩 사라져가는 추세라고 생각합니다.
-            하지만 우리는 간단한 게임으로도 강력한 중독성과 재미를 불러올 수 있음을 알고 있습니다.
-            웹에서의 게임 개발은 방해물이 아닌 도전과제임을 기억해야 합니다.
-            시작은 간단한 퍼즐게임에 불과하지만, 계속해서 발전해나가는 모습을 보일 것입니다.
-            이러한 뜻을 함께 할 분들은 <a onclick='link("dev/project_remind.php");'>Project REMIND 지원</a>을 참조해주십시오.
-            <h1 class="center">배워서 남주자.</h1>
-            개발자가 사용하는 시간 중 큰 비중을 차지하는 부분은 '구현'이라 말할 수 있습니다.
-            저 또한 게임을 개발하고 싶은 마음은 컸지만 어디서부터 시작해야 할지 몰라 참 난감하였던 경험이 있습니다.
-            그래서 저희 개발자들은 작은 노력을 추가했습니다.
-            저희의 도움이 큰 도움이 되지 못할지 언정, 저희가 배운 것을 조금이나마 나누기 위해 페이지의 소스 코드는 전부 게시글로 작성하여 공유됩니다.
-            <a onclick='link("dev");'>여기를</a> 눌러 개발자 지원 페이지로 이동하십시오.
-          </div>
-        </div>
-    </div>
-  </article>
-  <?php include_once('lib/footer.html'); ?>
-</body>
+		<link rel="shortcut icon" href="/assets/image/favicon.jpg">
+
+		<link rel="stylesheet" type="text/css" href="/assets/fonts/NanumGothic.css"/>
+		<link rel="stylesheet" type="text/css" href="/assets/fonts/Stalinist_One.css"/>
+
+		<link rel="stylesheet" type="text/css" href="/assets/css/lib/jquery.fullpage.min.css" />
+
+		<script type="text/javascript" src="/assets/js/lib/jquery.min.js"></script>
+		<script type="text/javascript" src="/assets/js/reset.js" defer></script>
+		<script type="text/javascript" src="/assets/js/lib/jquery.fullpage.min.js"></script>
+
+		<link rel="stylesheet" type="text/css" href="/assets/css/global/reset.css"/>
+		<link rel="stylesheet" type="text/css" href="/assets/css/global/header/header.css"/>
+		<link rel="stylesheet" type="text/css" media="screen and (max-width: 500px)" href="/assets/css/global/header/mobile.css"/>
+		<link rel="stylesheet" type="text/css" media="screen and (min-width: 501px)" href="/assets/css/global/header/desktop.css"/>
+
+		<link rel="stylesheet" type="text/css" href="/assets/css/lib/button.css"/>
+		<link rel="stylesheet" type="text/css" href="/assets/css/index/index.css"/>
+		<link rel="stylesheet" type="text/css" media="screen and (max-width: 500px)" href="/assets/css/index/mobile.css"/>
+		<link rel="stylesheet" type="text/css" media="screen and (min-width: 501px)" href="/assets/css/index/desktop.css"/>
+
+		<style>
+			#fullpage.focus-out {
+				animation-name: focus-out;
+				animation-duration: 0.4s;
+				animation-fill-mode: forwards;
+				animation-timing-function: cubic-bezier(0.1, 0.56, 0.8, 0.93);
+			}
+			@keyframes focus-out {
+				0% {
+					right: 0%;
+					opacity: 1;
+					filter: blur(0);
+				}
+				100% {
+					right: 120vw;
+					opacity: 0;
+					filter: blur(2px);
+				}
+			}
+			@media screen and (min-width: 501px) {
+				@keyframes focus-out {
+					0% {
+						right: 0%;
+						opacity: 1;
+						filter: blur(0);
+					}
+					100% {
+						right: 15vw;
+						opacity: 0;
+						filter: blur(2px);
+					}
+				}
+			}
+		</style>
+
+		<script type="text/javascript">
+			var colors = ['#282c37', '#9baec8', '#d9e1e8', '#6693B2'];
+
+			$(document).ready(function() {
+				$('#fullpage').fullpage({
+					sectionsColor: [colors[0], colors[1], colors[2], colors[3]],
+					lockAnchors:true,
+            		css3: true,
+            		scrollingSpeed: 800,
+					loopHorizontal: false,
+					controlArrows: false,
+					afterRender: function() { $('.slide').css('padding-top', '25px'); $('.slide').closest('.section').css('padding-top','0'); },
+				});
+			});
+
+			function link(str, colors_num) {
+				$('#fullpage').addClass("focus-out");
+				$('body').css('background', colors[colors_num]);
+				setTimeout(function(){
+					location.href = str;
+				}, 450);
+			}
+		</script>
+
+	</head>
+	<body>
+		<header>
+			<div id="box_left">
+				<a href="/">
+					<h1 id="title">REMIND</h1>
+				</a>
+			</div>
+			<div id="box_right">
+				<ul id="menu_member">
+					<li class="item item_member"><a href="/member/signup" class="link_member">회원가입</a></li>
+					<li class="item item_member"><a href="/member/login" class="link_member">로그인</a></li>
+				</ul>
+			</div>
+		</header>
+		<noscript>
+
+		</noscript>
+		<div id="fullpage" class="content">
+			<div class="section" style="color:white">
+				<h1 class="section_content subtitle">
+					<span class="font_logo">REMIND</span><br>
+					웹 게임의 발전 가능성을 제시하다
+				</h1>
+				<br>
+				<h4 class="section_details">
+					저는 어릴 적 즐겼던 플래시 게임을 떠올리며 웹 게임을 만들기 시작했습니다.
+					웹에서의 게임 개발은 방해물이 아닌 도전과제임을 기억해야 합니다.
+					시작은 간단한 퍼즐게임에 불과하지만, 계속해서 발전해나가는 모습을 보일 것입니다.
+				</h4>
+			</div>
+			<div class="section">
+				<h1 class="section_content subtitle">제작된 게임들을 둘러보세요</h1>
+				<br>
+				<h4 class="section_details">
+					Project REMIND의 팀원들이 만들어낸 멋진 게임들을 구경해보세요
+				</h4>
+				<br>
+				<div onclick='link("/board/games", 1)' class="button">자세히 보기</div>
+			</div>
+			<div class="section">
+				<h1 class="section_content subtitle">배워서 남주자</h1>
+				<br>
+				<h4 class="section_details">
+					무언가를 개발하고 싶은 마음은 컸지만 어디서부터 시작해야 할지 몰라 참 난감하였던 경험이 있을겁니다.
+					그래서 저희 개발자들은 이 페이지에 작은 노력을 추가했습니다.
+					이 페이지에 사용되는 모든 소스코드는 오픈소스로 공유됩니다.
+				</h4>
+				<br>
+				<div onclick='link("/board/developers", 2)' class="button">자세히 보기</div>
+			</div>
+			<div class="section">
+				<h1 class="section_content subtitle">당신도 함께 할 수 있습니다</h1>
+				<br>
+				<h4 class="section_details">
+					아이디어가 있는 사람, 코딩 실력이 있는 사람, 디자인 감각이 있는 사람들이 모여 팀을 꾸려 보세요.
+					간단한 퍼즐게임이라도 협업으로 친구들과, 아니면 해커톤같은 게임 개발, 시작해 보는 건 어떨까요?
+				</h4>
+				<br>
+				<div onclick='link("/board/project", 3)' class="button">자세히 보기</div>
+			</div>
+		</div>
+	</body>
 </html>
